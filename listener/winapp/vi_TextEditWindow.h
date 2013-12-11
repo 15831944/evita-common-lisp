070954d020191e4d3f07cbe0fe380fa906bebbbe
Eval <eval1749@gmail.com>
2013-12-11 21:23:34 +0900

    [Listener] Fix coding style in TextEditWindow.h

--- listener/winapp/vi_TextEditWindow.h
+++ listener/winapp/vi_TextEditWindow.h
@@ -31,178 +31,176 @@ enum DragMode
 //
 // TextEditWindow
 //
-//  Member Variables:
-//    m_pBlink
-//     A range contains the restore position after blinking matched
-//     parenthesis.
-//    m_pViewRange
-//     A range contains the start position of window.
+// Member Variables:
+// m_pBlink
+// A range contains the restore position after blinking matched
+// parenthesis.
+// m_pViewRange
+// A range contains the start position of window.
 //
-class TextEditWindow :
-    public CommandWindow_<TextEditWindow>,
-    public DoubleLinkedNode_<TextEditWindow>,
-    public DoubleLinkedNode_<TextEditWindow, Buffer>
-{
-    private: typedef DoubleLinkedNode_<TextEditWindow> WindowItem;
+class TextEditWindow
+    : public CommandWindow_<TextEditWindow>,
+      public DoubleLinkedNode_<TextEditWindow>,
+      public DoubleLinkedNode_<TextEditWindow, Buffer> {
+  private: typedef DoubleLinkedNode_<TextEditWindow> WindowItem;
 
-    protected: typedef Edit::Range  Range;
+  protected: typedef Edit::Range Range;
 
-    protected: struct AutoScroll
-    {
-        int     m_iDirection;
-        uint    m_nStartTick;
-        uint    m_nTimerId;
+  protected: struct AutoScroll {
+    int m_iDirection;
+    uint m_nStartTick;
+    uint m_nTimerId;
 
-        AutoScroll() :
-            m_iDirection(0),
-            m_nStartTick(0),
-            m_nTimerId(0) {}
+    AutoScroll()
+        : m_iDirection(0),
+          m_nStartTick(0),
+          m_nTimerId(0) {
+    }
 
-        void Continue(HWND);
-        void Start(HWND, int);
-        void Stop(HWND);
-    }; // AutoScroll
+    void Continue(HWND);
+    void Start(HWND, int);
+    void Stop(HWND);
+  };
 
-    protected: struct ScrollBar
-    {
-        HWND    m_hwnd;
-        int     m_nBar;
+  protected: struct ScrollBar {
+    HWND m_hwnd;
+    int m_nBar;
 
-        ScrollBar() :
-            m_hwnd(NULL),
-            m_nBar(SB_CTL) {}
+    ScrollBar()
+        : m_hwnd(nullptr),
+          m_nBar(SB_CTL) {
+    }
 
-        bool GetInfo(SCROLLINFO* pInfo)
-        {
-            if (NULL == m_hwnd) return false;
-            return 0 != ::GetScrollInfo(m_hwnd, m_nBar, pInfo);
-        } // GetInfo
+    bool GetInfo(SCROLLINFO* pInfo) {
+      return m_hwnd && ::GetScrollInfo(m_hwnd, m_nBar, pInfo);
+    }
 
-        HWND GetHwnd() const { return m_hwnd; }
+    HWND GetHwnd() const { return m_hwnd; }
 
-        void Set(HWND hwnd, int nBar)
-        {
-            m_hwnd = hwnd;
-            m_nBar = nBar;
-        } // Set
+    void Set(HWND hwnd, int nBar) {
+      m_hwnd = hwnd;
+      m_nBar = nBar;
+    }
 
-        void SetInfo(SCROLLINFO* pInfo, bool fRedraw)
-        {
-            if (NULL == m_hwnd) return;
-            ::SetScrollInfo(m_hwnd, m_nBar, pInfo, fRedraw);
-        } // SetInfo
-    }; // ScrollBar
+    void SetInfo(SCROLLINFO* pInfo, bool fRedraw) {
+      if (!m_hwnd)
+        return;
+      ::SetScrollInfo(m_hwnd, m_nBar, pInfo, fRedraw);
+    }
+  };
 
-    protected: DragMode     m_eDragMode;
-    protected: bool         m_fBlink;
-    protected: bool         m_fHasFocus;
-    private: const base::OwnPtr<gfx::Graphics> m_gfx;
-    protected: Posn         m_lCaretPosn;
-    protected: uint         m_nActiveTick;
-    protected: uint         m_nBlinkTimerId;
-    protected: int          m_nCharTick;
-    protected: AutoScroll   m_oAutoScroll;
-    protected: ScrollBar    m_oHoriScrollBar;
-    protected: ScrollBar    m_oVertScrollBar;
-    protected: Range*       m_pBlink;
-    protected: Page*        m_pPage;
-    // TODO(yosi): Manage life time of selection.
-    protected: Selection* selection_;
-    protected: Range*       m_pViewRange;
-    #if SUPPORT_IME
-    private: bool m_fImeTarget;
-    private: Posn m_lImeStart;
-    private: Posn m_lImeEnd;
-    #endif // SUPPORT_IME
-    protected: void*        m_pvHost;
-    protected: RECT         m_rc;
+  protected: DragMode m_eDragMode;
+  protected: bool m_fBlink;
+  protected: bool m_fHasFocus;
+  private: const base::OwnPtr<gfx::Graphics> m_gfx;
+  protected: Posn m_lCaretPosn;
+  protected: uint m_nActiveTick;
+  protected: uint m_nBlinkTimerId;
+  protected: int m_nCharTick;
+  protected: AutoScroll m_oAutoScroll;
+  protected: ScrollBar m_oHoriScrollBar;
+  protected: ScrollBar m_oVertScrollBar;
+  protected: Range* m_pBlink;
+  protected: Page* m_pPage;
+  // TODO(yosi): Manage life time of selection.
+  protected: Selection* selection_;
+  protected: Range* m_pViewRange;
+  #if SUPPORT_IME
+  private: bool m_fImeTarget;
+  private: Posn m_lImeStart;
+  private: Posn m_lImeEnd;
+  #endif // SUPPORT_IME
+  protected: void* m_pvHost;
+  protected: RECT m_rc;
 
-    // ctor/dtor
-    public: TextEditWindow(void* pvHost, Buffer*, Posn = 0);
-    public: ~TextEditWindow();
+  // ctor/dtor
+  public: TextEditWindow(void* pvHost, Buffer*, Posn = 0);
+  public: ~TextEditWindow();
 
-    // [A]
-    public: void Activate() { ::SetFocus(m_hwnd); }
+  // [A]
+  public: void Activate() { ::SetFocus(m_hwnd); }
 
-    // [B]
-    public: void Blink(Posn, uint);
+  // [B]
+  public: void Blink(Posn, uint);
 
-    // [C]
-    public:    Count ComputeMotion(Unit, Count, const gfx::PointF&, Posn*);
-    protected: Posn  computeGoalX(float, Posn);
+  // [C]
+  public: Count ComputeMotion(Unit, Count, const gfx::PointF&, Posn*);
+  protected: Posn computeGoalX(float, Posn);
 
-    // [E]
-    public:    Posn EndOfLine(Posn);
-    protected: Posn endOfLineAux(const gfx::Graphics&, Posn);
+  // [E]
+  public: Posn EndOfLine(Posn);
+  protected: Posn endOfLineAux(const gfx::Graphics&, Posn);
 
-    // [F]
-    protected: void format(const gfx::Graphics&, Posn);
+  // [F]
+  protected: void format(const gfx::Graphics&, Posn);
 
-    // [G]
-    public: uint       GetActiveTick() const { return m_nActiveTick; }
-    public: Buffer*    GetBuffer() const;
+  // [G]
+  public: uint GetActiveTick() const { return m_nActiveTick; }
+  public: Buffer* GetBuffer() const;
 
-    public: static   const char16* GetClass_()
-        { return L"TextEditWindow"; }
+  public: static const char16* GetClass_() { return L"TextEditWindow"; }
 
-    public: Count        GetColumn(Posn);
-    public: Posn         GetEnd();
+  public: Count GetColumn(Posn);
+  public: Posn GetEnd();
 
-    public: template<class T> T* GetHost() const
-        { return reinterpret_cast<T*>(m_pvHost); }
-        
-    public: TextEditWindow* GetNext() const
-        { return static_cast<const WindowItem*>(this)->GetNext(); }
+  public: template<class T> T* GetHost() const {
+    return reinterpret_cast<T*>(m_pvHost);
+  }
 
-    public: TextEditWindow* GetPrev() const
-        { return static_cast<const WindowItem*>(this)->GetPrev(); }
+  public: TextEditWindow* GetNext() const {
+    return static_cast<const WindowItem*>(this)->GetNext();
+  }
 
-    public: HWND       GetScrollBarHwnd(int) const;
-    public: Selection* GetSelection() const { return &*selection_; }
-    public: Posn       GetStart();
-    public: size_t     GetUndoSize() const;
+  public: TextEditWindow* GetPrev() const {
+    return static_cast<const WindowItem*>(this)->GetPrev();
+  }
 
-    // [H]
-    public: bool HasFocus() const { return m_fHasFocus; }
+  public: HWND GetScrollBarHwnd(int) const;
+  public: Selection* GetSelection() const { return &*selection_; }
+  public: Posn GetStart();
+  public: size_t GetUndoSize() const;
 
-    // [L]
-    public:  int LargeScroll(int, int, bool = true);
+  // [H]
+  public: bool HasFocus() const { return m_fHasFocus; }
 
-    // [M]
-    public: virtual Command::KeyBindEntry* MapKey(uint) override;
-    public: void MakeSelectionVisible();
-    public: Posn MapPointToPosn(const gfx::PointF point);
-    public: gfx::RectF MapPosnToPoint(Posn);
+  // [L]
+  public: int LargeScroll(int, int, bool = true);
 
-    // [O]
-    public:    virtual bool    OnIdle(uint);
-    protected: virtual LRESULT onMessage(UINT, WPARAM, LPARAM);
-    protected:   void          onVScroll(uint);
+  // [M]
+  public: virtual Command::KeyBindEntry* MapKey(uint) override;
+  public: void MakeSelectionVisible();
+  public: Posn MapPointToPosn(const gfx::PointF point);
+  public: gfx::RectF MapPosnToPoint(Posn);
 
-    // [R]
-    protected: void redraw();
-    protected: void redraw(bool);
-    protected: void render(const gfx::Graphics&);
+  // [O]
+  public: virtual bool OnIdle(uint);
+  protected: virtual LRESULT onMessage(UINT, WPARAM, LPARAM);
+  protected: void onVScroll(uint);
 
-    // [S]
-    protected: void selectWord(Posn);
-    public:    void SetScrollBar(HWND, int);
-    public:    int  SmallScroll(int, int);
-    public:    Posn StartOfLine(Posn);
-    protected: Posn startOfLineAux(const gfx::Graphics&, Posn);
-    private:   void stopDrag();
+  // [R]
+  protected: void redraw();
+  protected: void redraw(bool);
+  protected: void render(const gfx::Graphics&);
 
-    // [U]
-    protected: void updateScreen();
-    protected: void updateScrollBar();
+  // [S]
+  protected: void selectWord(Posn);
+  public: void SetScrollBar(HWND, int);
+  public: int SmallScroll(int, int);
+  public: Posn StartOfLine(Posn);
+  protected: Posn startOfLineAux(const gfx::Graphics&, Posn);
+  private: void stopDrag();
 
-    #if SUPPORT_IME
-    private: void onImeComposition(LPARAM);
-    public:  void Reconvert(Posn, Posn);
-    private: uint setReconvert(RECONVERTSTRING*, Posn, Posn);
-    private: BOOL showImeCaret(SIZE, POINT);
-    #endif // SUPPORT_IME
-}; // TextEditWindow
+  // [U]
+  protected: void updateScreen();
+  protected: void updateScrollBar();
+
+  #if SUPPORT_IME
+  private: void onImeComposition(LPARAM);
+  public: void Reconvert(Posn, Posn);
+  private: uint setReconvert(RECONVERTSTRING*, Posn, Posn);
+  private: BOOL showImeCaret(SIZE, POINT);
+  #endif // SUPPORT_IME
+};
 
 typedef DoubleLinkedList_<TextEditWindow, Buffer> WindowList;
 
