6f3e0a5e4fa13422347caf788168a901529630dc
Yoshifumi Inoue <yosin@chromium.org>
2013-12-11 15:32:03 +0900

    [Listener] Rename gfx::DpiHandler::pixel_in_dip_ to pixels_per_dip

--- listener/winapp/gfx_base.cpp
+++ listener/winapp/gfx_base.cpp
@@ -224,19 +224,19 @@ Brush::~Brush() {
 // DpiHandler
 //
 SizeF DpiHandler::AlignToPixel(const SizeF& size) const {
-  ASSERT(!pixel_in_dip_.is_empty());
-  return SizeF(MultipleOf(size.width, pixel_in_dip_.width),
-               MultipleOf(size.height, pixel_in_dip_.height));
+  ASSERT(!pixels_per_dip_.is_empty());
+  return SizeF(MultipleOf(size.width, pixels_per_dip_.width),
+               MultipleOf(size.height, pixels_per_dip_.height));
 }
 
 SizeF DpiHandler::CeilToPixel(const SizeF& size) const {
-  return SizeF(::ceilf(size.width * pixel_in_dip_.width),
-               ::ceilf(size.height * pixel_in_dip_.height));
+  return SizeF(::ceilf(size.width * pixels_per_dip_.width),
+               ::ceilf(size.height * pixels_per_dip_.height));
 }
 
 SizeF DpiHandler::FloorToPixel(const SizeF& size) const {
-  return SizeF(::floorf(size.width * pixel_in_dip_.width),
-               ::floorf(size.height * pixel_in_dip_.height));
+  return SizeF(::floorf(size.width * pixels_per_dip_.width),
+               ::floorf(size.height * pixels_per_dip_.height));
 }
 
 void DpiHandler::UpdateDpi(const SizeF& dpi) {
@@ -244,8 +244,8 @@ void DpiHandler::UpdateDpi(const SizeF& dpi) {
   // Note: All render targets except for ID2D1HwndRenderTarget, DPI values
   // are 96 DPI.
   float const default_dpi = 96.0f;
-  pixel_in_dip_ = dpi_ / default_dpi;
-  pixel_in_dip_ = SizeF(96.0f / dpi_.width, 96.0f / dpi_.height);
+  pixels_per_dip_ = dpi_ / default_dpi;
+  pixels_per_dip_ = SizeF(96.0f / dpi_.width, 96.0f / dpi_.height);
 }
 
 //////////////////////////////////////////////////////////////////////
